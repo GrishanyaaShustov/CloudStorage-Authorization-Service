b13d44766ac1aa788e7016da0d0bfef8dd1c5e88
Grigoriy <dinsuil275283@gmail.com>
2025-11-28 14:34:57 +0500

    Configure logger

--- cmd/authorization-service/main.go
+++ cmd/authorization-service/main.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"authorization-service/internal/config"
-	"fmt"
+	"authorization-service/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
 )
 
 func main() {
 	cfg, _ := config.MustLoad()
-	fmt.Println(*cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.Any("config", cfg))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "prod":
+		log = setupPrettySlog()
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
