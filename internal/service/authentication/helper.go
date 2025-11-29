9af9cb8e98779666b0930c91703f78c2c2bb5c3f
Grigoriy <dinsuil275283@gmail.com>
2025-11-29 20:30:38 +0500

    Registration method realization

--- internal/service/authentication/helper.go
+++ internal/service/authentication/helper.go
@@ -1,8 +1,19 @@
 package authentication
 
-import "google.golang.org/grpc/status"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc/status"
+)
 import "google.golang.org/grpc/codes"
 
 func statusUnimplemented(method string) error {
 	return status.Errorf(codes.Unimplemented, "%s is not implemented yet", method)
+}
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
 }
